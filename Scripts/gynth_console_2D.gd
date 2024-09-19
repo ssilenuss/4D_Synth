cd3b953f22dbab1392165854657439d7ac953530
JG <54590430+jls-has@users.noreply.github.com>
2024-09-19 11:44:04 -0400

    updated controls

--- Scripts/gynth_console_2D.gd
+++ Scripts/gynth_console_2D.gd
@@ -2,47 +2,82 @@ extends Control
 class_name Gynth_Controller
 var gynth : AudioOsc2D
 
-@onready var controls_visible_box : CheckBox = $VBoxContainer/Controls/VBoxContainer/HFO/CheckBox_Controls_Visible
-@onready var bang_box : CheckBox = $VBoxContainer/Controls/VBoxContainer/EnvelopeOptions/CheckBox_Bang
-@onready var gen_box : CheckBox = $VBoxContainer/Controls/VBoxContainer/HFO/CheckBox_Generating
-@onready var enable_envelope_box : CheckBox = $VBoxContainer/Controls/VBoxContainer/EnvelopeOptions/CheckBox_EnvelopeEnable
-@onready var loop_envelope_box : CheckBox = $VBoxContainer/Controls/VBoxContainer/EnvelopeOptions/CheckBox_LoopEnvelope
-@onready var wav_menu : MenuButton = $VBoxContainer/Controls/VBoxContainer/HFO/WaveType_Menu
-@onready var wav_vis : ColorRect = $VBoxContainer/wav_vis
-@onready var pitchlabel : Label = $VBoxContainer/Controls/VBoxContainer/PitchLabel
-@onready var pitchslider : HSlider = $VBoxContainer/Controls/VBoxContainer/PitchSlider
-@onready var limiter_slider : HSlider = $VBoxContainer/Controls/VBoxContainer/LimiterSlider
-@onready var attack_slider : HSlider = $VBoxContainer/Controls/VBoxContainer/AttackSlider
-@onready var decay_slider : HSlider = $VBoxContainer/Controls/VBoxContainer/DecaySlider
-@onready var sustain_slider: HSlider = $VBoxContainer/Controls/VBoxContainer/SustainSlider
-@onready var release_slider : HSlider = $VBoxContainer/Controls/VBoxContainer/ReleaseSlider
-@onready var speed_slider : HSlider = $VBoxContainer/Controls/VBoxContainer/SpeedSlider
-@onready var keyboard_box: CheckBox = $VBoxContainer/Controls/VBoxContainer/Keyboard_Controlle
+#@export var controls_visible_box : CheckBox 
+@export var bang_box : CheckBox 
+@export var gen_box : CheckBox 
+@export var enable_envelope_box : CheckBox 
+@export var loop_envelope_box : CheckBox 
+@export var wav_menu : MenuButton 
+@export var wav_vis : ColorRect 
+@export var frequency_label : LineEdit 
+@export var frequency_slider : HSlider 
+@export var limiter_label : LineEdit
+@export var limiter_slider : HSlider 
+@export var attack_slider : HSlider 
+@export var decay_slider : HSlider 
+@export var sustain_slider: HSlider 
+@export var release_slider : HSlider 
+@export var speed_slider : HSlider 
+@export var keyboard_box: CheckBox 
+@export var wav_controls : VBoxContainer
+@export var env_title : VBoxContainer
+@export var env_controls : VBoxContainer
+@export var speed_label : LineEdit
+@export var attack_label : LineEdit
+@export var decay_label : LineEdit
+@export var release_label : LineEdit
+@export var sustain_label : LineEdit
 
 @export var playhead_color: Color = Color(0,0,1,1)
-var playhead_position : float = 0.0
+
 
 @export var env_color : Color
 
 var keyboard_controlled := false
-var base_frequency : float :
-	set(value):
-		base_frequency= value
-		print("base frequency set: ", base_frequency, "gynth_frequency: ", gynth.frequency )
+var playhead_position : float = 0.0
+var wav_menu_popup : PopupMenu
+var base_frequency : float
+	
 
 func _ready() -> void:
 	#wav_menu.get_popup().id_pressed.connect(wavetype_selected)
 	gynth = $AudioOsc2D
 	wav_vis.gynth = gynth
 	base_frequency = gynth.frequency
+	
+	wav_menu_popup = wav_menu.get_popup()
+	wav_menu_popup.id_pressed.connect(_on_wav_menu_popup_selected)
+	_on_wav_menu_popup_selected(0)
 
-	enable_envelope_box.button_pressed = gynth.env_enabled
-	loop_envelope_box.button_pressed = gynth.env_enabled
-	_on_check_box_envelope_enable_toggled(gynth.env_enabled)
-	_on_check_box_loop_envelope_toggled(gynth.loop)
+	_on_frequency_text_submitted("440.0")
+	
+	_on_limiter_text_submitted("0.0")
+	
+	_on_check_box_envelope_enable_toggled(false)	
+	
+	_on_check_box_loop_envelope_toggled(false)
+
+	_on_check_box_generating_toggled(false)
+
+	_on_speed_text_submitted("1.0")
+	
+	_on_attack_line_submitted("0.5")
+	
+	_on_decay_text_submitted("0.5")
+	
+	_on_sustain_text_submitted("5.0")
+	
+	_on_release_text_submitted("1.0")
+
+
+
+	
 		
 func _on_check_box_generating_toggled(toggled_on: bool) -> void:
 	gynth.set_generating(toggled_on)
+	wav_controls.visible = toggled_on
+	env_title.visible = toggled_on
+	wav_vis.visible = toggled_on
 
 
 func _on_check_box_bang_button_down() -> void:
@@ -56,7 +91,7 @@ func init_menu()->void:
 	loop_envelope_box.button_pressed = gynth.loop
 	wav_menu.text = wav_menu.get_popup().get_item_text(gynth.osc_type)
 	wav_vis.gynth = gynth
-	pitchslider.value = gynth.pitch
+	frequency_slider.value = gynth.pitch
 	_on_pitch_slider_value_changed(gynth.pitch)
 	limiter_slider.value = gynth.limiter
 	attack_slider.value = gynth.attack
@@ -68,62 +103,156 @@ func init_menu()->void:
 
 func _on_check_box_envelope_enable_toggled(toggled_on: bool) -> void:
 	gynth.set_env_enabled(toggled_on)
+	env_controls.visible = toggled_on
+	enable_envelope_box.button_pressed = toggled_on
+	_on_check_box_generating_toggled(false)
+	_on_check_box_generating_toggled(true)
 
 
 func _on_check_box_loop_envelope_toggled(toggled_on: bool) -> void:
 	gynth.loop = toggled_on
+	loop_envelope_box.button_pressed = toggled_on
 
 
-func wavetype_selected(idx: int)->void:
-	wav_menu.text = wav_menu.get_popup().get_item_text(idx)
-	gynth.set_generating(false)
-	gynth.osc_type = idx
-	gynth.set_generating(true)
-	
+#func wavetype_selected(idx: int)->void:
+	#wav_menu.text = wav_menu.get_popup().get_item_text(idx)
+	#gynth.set_generating(false)
+	#gynth.osc_type = idx
+	#gynth.set_generating(true)
+	#
 
 
 func _on_pitch_slider_value_changed(value: float) -> void:
 	#gynth.set_effective_frequency(value)
-	gynth.pitch = value
+	gynth.pitch = value-0.01
 	
 	base_frequency = gynth.get_effective_frequency()
-	pitchlabel.text = "Pitch: " + str(base_frequency)
+	frequency_label.text = str(base_frequency)
 
 func _on_keyboard_pressed(pitch_mod: float) -> void:
 	if keyboard_controlled:
-		var new_frequncy : float = base_frequency * pow(2, pitch_mod)
-		gynth.set_effective_frequency(new_frequncy)
+		var new_frequency : float = base_frequency * pow(2, pitch_mod)
+		gynth.set_effective_frequency(new_frequency)
 		#gynth.pitch = base_pitch+new_pitch
-		pitchlabel.text = "Pitch: " + str(gynth.get_effective_frequency())
+		frequency_label.text = str(gynth.get_effective_frequency())
 		
 func _on_limiter_slider_value_changed(value: float) -> void:
 	gynth.limiter = value
+	limiter_label.text = str(value)
 
 
 func _on_attack_slider_value_changed(value: float) -> void:
 	gynth.attack = value
+	attack_label.text = str(value)
 
 func _on_decay_slider_value_changed(value: float) -> void:
 	gynth.decay = value
+	decay_label.text = str(value)
 
 
 func _on_sustain_slider_value_changed(value: float) -> void:
 	gynth.sustain = value
+	sustain_label.text = str(value)
 
 
 func _on_release_slider_value_changed(value: float) -> void:
 	gynth.release = value
+	release_label.text = str(value)
 
 
 func _on_speed_slider_value_changed(value: float) -> void:
 	gynth.speed = value
+	speed_label.text = str(value) + "(s)"
 
-
-func _on_controls_visible_box_toggled(toggled_on: bool) -> void:
-	$Controls.visible = false
+#
+#func _on_controls_visible_box_toggled(toggled_on: bool) -> void:
+	#$Controls.visible = false
 	
-
+func _on_wav_menu_popup_selected(id)->void:
+	wav_menu.text = "WAVE: " + wav_menu_popup.get_item_text(id)
+	gynth.osc_type = id
+	_on_check_box_generating_toggled(false)
+	_on_check_box_generating_toggled(true)
 
 func _on_keyboard_controlle_toggled(toggled_on: bool) -> void:
 	keyboard_controlled = toggled_on
-	print(keyboard_controlled)
+
+
+func _on_frequency_text_submitted(new_text: String) -> void:
+	var old_text = frequency_label.text
+	if new_text.is_valid_float():
+		old_text = new_text
+		frequency_label.text = new_text
+		gynth.set_effective_frequency(float(new_text))
+		frequency_slider.value = gynth.pitch_scale
+	else:
+		frequency_label.text = str(gynth.get_effective_frequency())
+	
+
+
+func _on_limiter_text_submitted(new_text: String) -> void:
+	var old_text = limiter_label.text
+	if new_text.is_valid_float():
+		old_text = new_text
+		limiter_label.text = new_text
+		limiter_slider.value = float(new_text)
+		
+	else:
+		limiter_label.text = str(limiter_slider.value)
+		
+	
+
+
+func _on_speed_text_submitted(new_text: String) -> void:
+	var old_text = speed_label.text
+	if new_text.is_valid_float():
+		old_text = new_text
+		speed_label.text = new_text
+		speed_slider.value = float(new_text)
+		
+	else:
+		speed_label.text = str(speed_slider.value)
+
+
+func _on_attack_line_submitted(new_text: String) -> void:
+	var old_text = attack_label.text
+	if new_text.is_valid_float():
+		old_text = new_text
+		attack_label.text = new_text
+		attack_slider.value = float(new_text)
+		
+	else:
+		attack_label.text = str(attack_slider.value)
+
+
+func _on_decay_text_submitted(new_text: String) -> void:
+	var old_text = decay_label.text
+	if new_text.is_valid_float():
+		old_text = new_text
+		decay_label.text = new_text
+		decay_slider.value = float(new_text)
+		
+	else:
+		decay_label.text = str(decay_slider.value)
+
+
+func _on_release_text_submitted(new_text: String) -> void:
+	var old_text = release_label.text
+	if new_text.is_valid_float():
+		old_text = new_text
+		release_label.text = new_text
+		release_slider.value = float(new_text)
+		
+	else:
+		release_label.text = str(release_slider.value)
+
+
+func _on_sustain_text_submitted(new_text: String) -> void:
+	var old_text = sustain_label.text
+	if new_text.is_valid_float():
+		old_text = new_text
+		sustain_label.text = new_text
+		sustain_slider.value = float(new_text)
+		
+	else:
+		sustain_label.text = str(sustain_slider.value) + "(s)"
