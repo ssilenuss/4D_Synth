9f6b379df1bb91803c4215c46e9ae5e995218421
jlsgangwisch <jlsgangwisch@gmail.com>
2026-01-20 14:57:45 -0500

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# 4D_Synth
-A Basic Synth for 4D Foundations at the Hartford Art School
--- Scenes/lowerhalf.gd
+++
@@ -1,5 +0,0 @@
-extends VBoxContainer
-
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("4"):
-		visible = !visible
